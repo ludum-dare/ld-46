2f81f930ac48db6d72a8c9bd76603f0c03f2e9ec
Stephen Lafortune <slafor24@gmail.com>
2020-04-19 20:23:27 -0400

    fixed jump sound tab

--- player1.gd
+++ player1.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	elif AUTO_JUMP and is_on_floor():
 		print("AutoJumped")
 		velocity.y -= 600
-    $JumpSound.play()
+		$JumpSound.play()
 	if position.x <= 0 and velocity.x < 0:
 		position.x = get_viewport().get_visible_rect().size[0]
 	if position.x >= get_viewport().get_visible_rect().size[0] and velocity.x > 0:
